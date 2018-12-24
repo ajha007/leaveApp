5fbabb29967e2f4f0425f7271d4acb442b782d2d
Amit Kumar Jha <ajha@aapnainfotech.com>
2018-12-24 18:05:20 +0530

    Signed-off-by: Amit Kumar Jha <ajha@aapnainfotech.com>

--- src/components/stateless/Dashboard/Dashboard.js
+++ src/components/stateless/Dashboard/Dashboard.js
@@ -3,50 +3,50 @@ import Holidays from '../Holidays/Holidays';
 
 const Dashboard = () => {
     return(
-        <div class="my-3 my-md-5">
-            <div class="container">
-                <div class="page-header">
-                    <h1 class="page-title">
+        <div className="my-3 my-md-5">
+            <div className="container">
+                <div className="page-header">
+                    <h1 className="page-title">
                         Dashboard
                     </h1>
                 </div>
-                <div class="row row-cards">
+                <div className="row row-cards">
                    
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="card p-3">
-                            <div class="d-flex align-items-center">
-                                <span class="stamp stamp-md bg-red mr-3">
-                                    <i class="fe fe-users"></i>
+                    <div className="col-sm-6 col-lg-3">
+                        <div className="card p-3">
+                            <div className="d-flex align-items-center">
+                                <span className="stamp stamp-md bg-red mr-3">
+                                    <i className="fe fe-users"></i>
                                 </span>
                                 <div>
-                                    <h4 class="m-0"><a href="javascript:void(0)">1,352 <small>Members</small></a></h4>
-                                    <small class="text-muted">163 registered today</small>
+                                    <h4 className="m-0"><a href="javascript:void(0)">1,352 <small>Members</small></a></h4>
+                                    <small className="text-muted">163 registered today</small>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="card p-3">
-                            <div class="d-flex align-items-center">
-                                <span class="stamp stamp-md bg-yellow mr-3">
-                                    <i class="fe fe-message-square"></i>
+                    <div className="col-sm-6 col-lg-3">
+                        <div className="card p-3">
+                            <div className="d-flex align-items-center">
+                                <span className="stamp stamp-md bg-yellow mr-3">
+                                    <i className="fe fe-message-square"></i>
                                 </span>
                                 <div>
-                                    <h4 class="m-0"><a href="javascript:void(0)">132 <small>Comments</small></a></h4>
-                                    <small class="text-muted">16 waiting</small>
+                                    <h4 className="m-0"><a href="javascript:void(0)">132 <small>Comments</small></a></h4>
+                                    <small className="text-muted">16 waiting</small>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-12 col-md-12 col-sm-6 col-lg-3">
+                <div className="col-lg-12 col-md-12 col-sm-6 col-lg-3">
                     <Holidays />        
                 </div>
-                <div class="row row-cards row-deck">
+                <div className="row row-cards row-deck">
                     
                     
                 </div>
-                <div class="row row-cards row-deck">
+                <div className="row row-cards row-deck">
                     
                 </div>
             </div>
