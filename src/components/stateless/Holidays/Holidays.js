7b2e9cdb4af9fcb45b118b2ae7658bcdd8984769
ajha007 <amitjha.vns@gmail.com>
2018-12-25 23:31:16 +0530

    Update new data table and add pageUrl prop in navItem.

--- src/components/stateless/Holidays/Holidays.js
+++ src/components/stateless/Holidays/Holidays.js
@@ -1,59 +1,101 @@
 import React from 'react';
 import SortableTbl from 'react-sort-search-table';
+import Table from 'react-responsive-data-table';
+
 
 let MyData = 
 [
     {
-        "_id"   : "srno_1",
+        "id"   : "srno_1",
         "name"  : "New Year’s Day",
         "date"  : "01 Jan 2019",
         "day"   : "Tuesday",        
     },
     {
-        "_id"   : "srno_2",
+        "id"   : "srno_2",
         "name"  : "Republic Day",
         "date"  : "26 Jan 2019",
         "day"   : "Saturday",
     },
     {
-        "_id"   : "srno_3",
+        "id"   : "srno_3",
         "name"  : "Holi",
         "date"  : "21 Mar 2019",
         "day"   : "Thursday",
     },
     {
-        "_id"   : "srno_4",
+        "id"   : "srno_4",
         "name"  : "Good Friday",
         "date"  : "19 April 2019",
         "day"   : "Friday",
     },
     {
-        "_id"   : "srno_5",
+        "id"   : "srno_5",
         "name"  : "Floating holiday: Any one Id-Ul- Fitre/Maha Navami/Goverdhan Puja/Guruparv",
         "date"  : "5th June/7th Oct/28th Oct/12th Nov",
         "day"   : "Wednesday/Monday/Monday/Tuesday",
     },
 ];
 
+const header = [
+    {
+        id:"ID",
+        name: "Holiday",
+        date: "Date",
+        day: "Day"
+    }
+];
+
 const holidaysList = (props) =>{
 	let col = [
+        "id",
         "name",
         "date",
         "day",
     ];
 	let tHead = [
+        "ID",
         "Holiday",
         "Date",
         "Day",
     ];
 
+
     return (
-		<SortableTbl tblData={MyData}
-			tHead={tHead}
-			dKey={col}
-			search={true}
-			defaultCSS={true}			
-		/>
+		// <SortableTbl tblData={MyData}
+		// 	tHead={tHead}
+		// 	dKey={col}
+		// 	search={true}
+		// 	defaultCSS={true}			
+        // />
+        
+        <Table style={{
+            opacity: 0.8,
+            backgroundColor: "blue",
+            color: "#ffffff",
+            textAlign: "center"
+          }}
+          tableStyle="table table-hover table-striped table-bordered table-borderless table-responsive"
+          pages={true}
+          pagination={true}
+          onRowClick={() => {}} // if You Want Table Row Data OnClick then assign this {row => console.log(row)}
+          page={true}
+          errormsg="Error. . ."
+          loadingmsg="Loading. . ."
+          isLoading={false} 
+          sort={true} 
+          title="Customers"
+          search={true}
+          size={10}
+          data={{
+            head: {
+                id: "ID",
+                name: "Name",
+                date: "Date",
+                day: "Day At",
+            },
+            data:MyData
+          }} />
 );
 }
 
